40f715f7a92cda6ce10a920b2b016622d7f4b07e
CodeByLoredana <loredana.cebotari06@e-uvt.ro>
2026-01-19 18:19:48 +0000

    Project

--- tests.py
+++ tests.py
@@ -1,13 +1,24 @@
 #import all needed modules here
 import unittest
+from app import interpret
+
 
 #write all your tests below this line
+class TestInterpret(unittest.TestCase):
+    def test_low(self):
+        self.assertEqual(interpret(3), "Low")
 
+    def test_moderate(self):
+        self.assertEqual(interpret(7), "Moderate")
+
+    def test_high(self):
+        self.assertEqual(interpret(12), "High")
 
 #write your test suite here, in the main() function
 def main():
     #call all your tets here, one on each line
     print("Starting tests suite...")
+    unittest.main()
     
 #please do not change the lines below
 if __name__ == "__main__":
